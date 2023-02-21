4b21a65cbbc2b47e5a690a5aba8a12f2214bb4cf
I.Muratov <ivanmuratov.2016@gmail.com>
2023-02-21 20:54:42 +0200

    generate users endpoint added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, request
+from faker import Faker
 
 app = Flask(__name__)
 
@@ -9,3 +9,18 @@ def requirements():
     with open('requirements.txt') as reqs:
         return reqs.read()
 
+
+@app.route("/generate-users/")
+def user_dict():
+    fake = Faker()
+    users = {}
+    quantity = 100
+    args = request.args
+    if args.get('quantity'):
+        quantity = int(args.get('quantity'))
+    for i in range(quantity):
+        full_name = fake.name()
+        first_name = full_name.split()[0]
+        last_name = full_name.lower().split()[1]
+        users[first_name] = f'{last_name}@mail.com'
+    return users
