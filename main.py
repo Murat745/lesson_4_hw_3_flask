3f6843480813bfe93517c2280ccff2489a3c6b9b
I.Muratov <ivanmuratov.2016@gmail.com>
2023-02-22 16:21:36 +0200

    requirements.txt modified

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from faker import Faker
+import pandas as pd
 import requests
 
 
@@ -11,9 +12,9 @@ def learning_flask():
     return "<p> <h1>Here I'm going to learn Flask</h1> </p>"
 
 
-@app.route('/requirements/')
+@app.route("/requirements/")
 def requirements():
-    with open('requirements.txt') as reqs:
+    with open("requirements.txt") as reqs:
         return reqs.read()
 
 
@@ -23,17 +24,26 @@ def user_dict():
     users = {}
     quantity = 100
     args = request.args
-    if args.get('quantity'):
-        quantity = int(args.get('quantity'))
+    if args.get("quantity"):
+        quantity = int(args.get("quantity"))
     for i in range(quantity):
         full_name = fake.name()
         first_name = full_name.split()[0]
         last_name = full_name.lower().split()[1]
-        users[first_name] = f'{last_name}@mail.com'
+        users[first_name] = f"{last_name}@mail.com"
     return users
 
 
-@app.route("/space/", methods=['GET'])
+@app.route("/mean/")
+def average():
+    data = pd.read_csv("hw.csv")
+    data.columns = ["Index", "Height(Inches)", "Weight(Pounds)"]
+    avr_height_cm = data.mean()[1]
+    avr_weight_kg = data.mean()[2]
+    return f"Average height: {avr_height_cm}\nAverage weight: {avr_weight_kg}"
+
+
+@app.route("/space/")
 def cosmonauts():
     r = requests.get('http://api.open-notify.org/astros.json')
     result = r.json()['number']
