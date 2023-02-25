80bd3f757213d922dd56ab9b2796013db60e78ac
I.Muratov <ivanmuratov.2016@gmail.com>
2023-02-25 19:39:07 +0200

    schema.sql modified

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,7 +2,6 @@ CREATE TABLE tracks(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(100),
     artist VARCHAR(100),
+    genre VARCHAR(100),
     length INTEGER,
-    genre_id INTEGER NOT NULL,
-    FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
